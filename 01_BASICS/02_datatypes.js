6f2d69ab43db4966560ce1cdcc3bfc3bd31f84bd
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-02 11:29:33 +0000

    datatypes done

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all js code as newer version of JS
+
+// alert(3+3) we are using node js, not browser
+
+
+let name = "Yashpal" 
+let isloggedin = true
+let state = null;
+
+//number => 2 to power 53
+
+//bigint   
+
+//bolean => true/false
+
+//null => empty value
+
+//undefined => variable is declared but not assigned any value
+
+//symbol => unique value, not used in this course
+
+//object => collection of key value pairs
+
+
+console.log(typeof name); // 
+console.log(typeof null)
+
+console.log(typeof undefined);
+console.log(typeof null); 
