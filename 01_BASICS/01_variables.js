ec2fe4560ed92ace9818b63303e6d08e371e78c6
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-02 10:58:04 +0000

    container modified

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,15 @@
+const account_id = 14453
+let accountEmail = "yashpal@1904@gmail.com"
+var accountPassword = "12345"
+// account_id = 2
+
+accountEmail = "hc"
+accountCity = "Jaipur"
+accountPassword = "12365"
+console.log(account_id);
+
+console.table([accountEmail,accountPassword,accountCity])
+
+let accountState;
+
+console.log(accountState);
