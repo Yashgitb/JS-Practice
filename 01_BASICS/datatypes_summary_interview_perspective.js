11f1dd70b342574fb19b7634d2dd136d029fe3a9
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-03 13:52:48 +0000

    summary done

--- 01_BASICS/datatypes_summary_interview_perspective.js
+++ 01_BASICS/datatypes_summary_interview_perspective.js
@@ -16,3 +16,51 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 
 console.log(id === anotherId);
+
+
+const bigNumber = 7527639092020n
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+const heros = [" shaktiman ",  " naagraj ", " doga "]
+
+
+
+let myObj = {
+
+   age:22,
+   name:"Yash"
+
+}
+
+const myfunction = function(){
+
+    console.log("Hello World");
+    
+}
+
+
+
+
+console.log(myfunction);
+
+console.log(myObj);
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
