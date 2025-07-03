5578c86259d58999a1471935c7205d437e18fcaa
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-03 11:36:33 +0000

    comaprisons done

--- 01_BASICS/03_datatype_conversion.js
+++ 01_BASICS/03_datatype_conversion.js
@@ -23,11 +23,42 @@
 // let booleanisloggedIn = Boolean(isloggedIn) 
 // console.log(booleanisloggedIn)
 
-let sum = 33
-let stringNUmber = String(sum)
-console.log(sum);
-console.log(typeof(sum));
+// let sum = 33
+// let stringNUmber = String(sum)
+// console.log(sum);
+// console.log(typeof(sum));
 
+//*******************Operations *************************/
+
+
+
+let value = 3
+
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Yash"
+let str2 = " Pal"
+let str3 = str1 +str2
+
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+ 2 + 2)
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+gameCounter++
+
+console.log(gameCounter);
 
 
 
---
+++ 01_BASICS/04_comparisons.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+let str1 = "yash"
+let str2 = "pal"
+console.log(str1 > str2);
+
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0);
+
+console.log(undefined  == 0);
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// ===
+console.log("2" === 2)
+let name = "Yash"
+console.log("yash" === "yash");
+console.log("yash" === name);
+
+// It checks the data type as well text
+// do not do comparison with different data types
+
+
