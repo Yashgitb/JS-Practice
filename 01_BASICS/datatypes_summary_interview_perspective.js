efd1a29e5ef1a3106980b34be8e711c5b889f8a9
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-03 13:37:30 +0000

    datatypes summary

---
+++ 01_BASICS/datatypes_summary_interview_perspective.js
@@ -0,0 +1,18 @@
+//Primitive data types -- which are apsses dy value
+
+// 7 types : String , Number , Boolean , Null , BigInt, Symbol , undefined
+
+
+//Non Primitive data types  -- which are passed by reference
+
+//Arrays, Objects, Functions
+
+
+const outsidetemp = null
+const isLoggedIn = false
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
