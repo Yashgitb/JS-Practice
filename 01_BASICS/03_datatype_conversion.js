81ecef9a711a2a09bbc8f246c6bcc3944a0c6e13
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-03 10:42:46 +0000

    conversion of data types

---
+++ 01_BASICS/03_datatype_conversion.js
@@ -0,0 +1,32 @@
+// let score = "yash"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score);
+
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN Not a Number
+//true => 1
+//null => 0
+//undefined => NaN
+
+// let isloggedIn = "Yash"
+// let isloggedIn = 1 
+
+// let booleanisloggedIn = Boolean(isloggedIn) 
+// console.log(booleanisloggedIn)
+
+let sum = 33
+let stringNUmber = String(sum)
+console.log(sum);
+console.log(typeof(sum));
+
+
+
