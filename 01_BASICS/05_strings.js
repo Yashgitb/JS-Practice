f19c7b5f2f8dd0abe7243563df97a0f3d6461fe3
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-05 03:05:46 +0000

    strings done

---
+++ 01_BASICS/05_strings.js
@@ -0,0 +1,29 @@
+const gameName = new String("Yash Pal");
+
+console.log(gameName)
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('Y'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+
+newString1 = "              Yash       "
+console.log(newString1)
+console.log(newString1.trim())
+
+
+const url = "https://yashpal.com"
+
+console.log(url.replace(".com",".in"))
+console.log(url.includes("yash"))
+
+console.log(gameName.split(" "));
+
+
+
+
+
