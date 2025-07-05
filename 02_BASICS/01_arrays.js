a11ff065aff0f2a6413128341a17f7a2e9c0e8b5
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-05 10:32:48 +0000

    arr

---
+++ 02_BASICS/01_arrays.js
@@ -0,0 +1,41 @@
+//array
+
+
+const myArr = [0,1,2,3,4,5];
+
+console.log(myArr[0]);
+
+
+//Array Methods 
+console.log(myArr)
+
+myArr.push(6)
+myArr.push(7)
+
+console.log(myArr)
+myArr.pop()
+myArr.pop()
+
+
+console.log(myArr)
+
+myArr.unshift(9)
+
+console.log(myArr)
+
+myArr.shift()
+
+console.log(myArr.includes(9))
+myArr.unshift(9)
+console.log(myArr);
+
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(3));
+
+
+const newArr = myArr.join()
+console.log(myArr)
+console.log(typeof newArr);
+
+// slice , splice
+
