7db7fa36ea626b1aa67f7f568bfe273586bb1ef9
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-06 17:57:18 +0000

    obj

---
+++ 02_BASICS/01_objects.js
@@ -0,0 +1,20 @@
+let obj = new Object;
+
+obj.firstname ="Yash";
+obj.lastname ="Pal";
+obj.age = 50;
+obj.gender = "Male";
+
+console.log(obj);
+
+let obj1 = {};
+obj1.firstname ="Yash";
+obj1.lastname ="Pal";
+obj1.age = 50;
+obj1.gender = "Male";
+
+console.log(obj1);
+
+
+
+
