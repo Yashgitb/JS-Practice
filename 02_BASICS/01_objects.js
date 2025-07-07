8bc13fecf6cb4659b17c4bdd996c9ce67d03b0dc
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-07 16:56:57 +0000

    object methods

--- 02_BASICS/01_objects.js
+++ 02_BASICS/01_objects.js
@@ -16,5 +16,33 @@ obj1.gender = "Male";
 console.log(obj1);
 
 
+let person = {
 
+firstName: "Yash",
+lastName: "Pal",
+age: 50    
+}
 
+let x = person;
+
+x.age = 10;
+console.log(person);
+
+// Object Properties
+
+person.nationality = "Indian";
+
+console.log(person);
+
+delete person.nationality;
+
+console.log(person);
+
+//Nested onjects
+person.myCars = {
+    Car1:"Nexon",
+    Car2:"Punch",
+    Car3:"Dzire"
+}
+
+console.log(person.myCars.Car2);
