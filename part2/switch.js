f221c4ab8c4ce59ad1be184e90ffbe7871515b88
Yashgitb <162584347+Yashgitb@users.noreply.github.com>
2025-07-14 15:55:03 +0000

    switch done

---
+++ part2/switch.js
@@ -0,0 +1,19 @@
+let color = "green";
+
+switch (color) {
+    case "red":
+        console.log("Stop");
+        
+        break;
+    case "green":
+        console.log("Slow down");
+        
+        break;
+    case "yellow":
+
+        console.log("GO"); 
+        break;
+    default:
+        console.log("Broken-light");
+
+}
